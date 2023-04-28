14ed8477b614218c01ee531bb98e51049982a76b
ZakariaPZ <zaaabc123@gmail.com>
2023-04-28 19:07:10 -0400

    Added initial callbacks

---
+++ callbacks.py
@@ -0,0 +1,12 @@
+import lightning as pl
+import torch
+
+class MyCallback(pl.Callback):
+    def on_train_epoch_end(self, trainer, pl_module):
+        # do something with all training_step outputs, for example:
+        epoch_mean = torch.stack(pl_module.training_step_outputs).mean()
+        pl_module.log("training_epoch_mean", epoch_mean)
+        # free up the memory
+        pl_module.training_step_outputs.clear()
+
+    
