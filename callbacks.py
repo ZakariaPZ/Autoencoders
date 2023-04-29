3baa4fb2788614e3aa495087e0f18be6e6814bf4
ZakariaPZ <zaaabc123@gmail.com>
2023-04-29 00:50:35 -0400

    Fixed callback

--- callbacks.py
+++ callbacks.py
@@ -1,7 +1,7 @@
 import lightning as pl
 import torch
 
-class MyCallback(pl.Callback):
+class LossCallback(pl.Callback):
     def on_train_epoch_end(self, trainer, pl_module):
         # do something with all training_step outputs, for example:
         epoch_mean = torch.stack(pl_module.training_step_outputs).mean()
