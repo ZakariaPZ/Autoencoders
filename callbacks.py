e89362481aab53321e40a8835eb0bcb51a56bf78
ZakariaPZ <zaaabc123@gmail.com>
2023-04-30 11:37:59 -0400

    Added reconstruction visualization

--- callbacks.py
+++ callbacks.py
@@ -1,12 +1,21 @@
+from typing import Any, Optional
 import lightning as pl
+import lightning.pytorch as pl
+from lightning.pytorch.utilities.types import STEP_OUTPUT
 import torch
+import typing as th
 
 class LossCallback(pl.Callback):
-    def on_train_epoch_end(self, trainer, pl_module):
-        # do something with all training_step outputs, for example:
-        epoch_mean = torch.stack(pl_module.training_step_outputs).mean()
-        pl_module.log("training_epoch_mean", epoch_mean)
-        # free up the memory
-        pl_module.training_step_outputs.clear()
-
+    def on_validation_batch_end(self, 
+                                trainer: pl.Trainer, 
+                                pl_module: pl.LightningModule, 
+                                outputs: th.Union[STEP_OUTPUT, None], 
+                                batch: Any, 
+                                batch_idx: int, 
+                                dataloader_idx: int = 0) -> None:
     
+        x, _ = batch
+        image = x[batch_idx]
+        reconstruction = pl_module(image.unsqueeze(0))
+        pl_module.logger.experiment.add_image('Original', image, 0)
+        pl_module.logger.experiment.add_image('Reconstruction', reconstruction.squeeze(0), 1)
